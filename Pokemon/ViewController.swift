3249b661966011a597429d9d5c469a71f8649367
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2024-12-24 18:27:32 +0900

    Initial Commit

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by 김하민 on 12/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
