6294ebc31f8dca3186b8841c3caae4cf1dfebb9f
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2024-12-26 16:38:28 +0900

    [chores] file name changed ViewController >> MainViewController

--- Pokemon/App/SceneDelegate.swift
+++ Pokemon/App/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         
-        let mainVC = ViewController()
+        let mainVC = MainViewController()
         window?.rootViewController = mainVC
         window?.makeKeyAndVisible()
     }
