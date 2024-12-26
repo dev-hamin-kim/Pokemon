4e98d0b41c2ea98fbaa27100bc8ae4a722473e1f
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2024-12-26 14:15:59 +0900

    [new] Lv.1 requirements completed created NetworkManager

---
+++ Pokemon/Common/NetworkManager.swift
@@ -0,0 +1,49 @@
+//
+//  NetworkManager.swift
+//  Pokemon
+//
+//  Created by 김하민 on 12/24/24.
+//
+
+import Foundation
+import RxSwift
+
+enum NetworkError: Error {
+    case invalidURL
+    case failedToFetchData
+    case failedToDecode
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetch<T: Decodable>(url: URL) -> Single<T> {
+        return Single.create { observer in
+            let session = URLSession(configuration: .default)
+            session.dataTask(with: URLRequest(url: url)) { data, response, error in
+                if let error = error {
+                    observer(.failure(error))
+                    return
+                }
+                
+                guard let data = data,
+                      let response = response as? HTTPURLResponse,
+                      (200...300).contains(response.statusCode) else {
+                    observer(.failure(NetworkError.failedToFetchData))
+                    return
+                }
+                
+                do {
+                    let decodedData = try JSONDecoder().decode(T.self, from: data)
+                    observer(.success(decodedData))
+                } catch {
+                    observer(.failure(NetworkError.failedToDecode))
+                }
+            }.resume()
+            
+            return Disposables.create()
+        }
+    }
+}
